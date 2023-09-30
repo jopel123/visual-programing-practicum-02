cbddce5e9b2e101c2d4558107d36babe8bced37e
JOPEL SIMARMATA <70961458+jopel123@users.noreply.github.com>
2023-09-30 18:28:26 +0700

    p02

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,36 @@
-// NIM - Your Name
+// 12S23036 - Jopel Simarmata
+import java.util.*;
+import java.lang.Math;
+
+class P02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String teks, nIM, studi, nama, tanggaljam, dosen, ket, status;
+        int prioritas, tingkatkesulitan, harideadline;
+
+        teks = input.nextLine();
+        nIM = input.nextLine();
+        studi = input.nextLine();
+        dosen = input.nextLine();
+        tanggaljam = input.nextLine();
+        nama = input.nextLine();
+        tingkatkesulitan = input.nextInt();
+        harideadline = input.nextInt();
+        status = input.nextLine();
+        prioritas = (int) (tingkatkesulitan * (1.0 / harideadline));
+        if (prioritas > 3) {
+            ket = "Penting! Anda harus mengerjakan tugas ini segera.";
+        } else {
+            if (prioritas <= 3 && prioritas > 1.5) {
+                ket = "Tugas ini memiliki prioritas menengah.";
+            } else {
+                if (prioritas < 1.5) {
+                    ket = "Tugas ini relatif ringan, namun jangan tunda terlalu lama.";
+                }
+            }
+        }
+        System.out.println("Prioritas : " + prioritas);
+        System.out.println("" + teks + "|" + nIM + "|" + studi + "|" + dosen + "|" + tanggaljam + "|" + nama + "|" + status + "|" + ket);
+    }
+}
--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,33 @@
-//NIM -Nama
+//12S23036 - Jopel Simarmata
+import java.util.*;
+import java.lang.Math;
+
+class P02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama, aC, lantai, ketersediaan, status;
+        int kapasitas, jumlah, skor;
+
+        nama = input.nextLine();
+        kapasitas = input.nextInt();
+        jumlah = input.nextInt();
+        aC = input.nextLine();
+        lantai = input.nextLine();
+        ketersediaan = input.nextLine();
+        skor = (int) ((double) jumlah / kapasitas * 100);
+        if (skor >= 40) {
+            status = "Gudang Elite";
+        } else {
+            if (skor >= 25) {
+                status = "Gudang Standar";
+            } else {
+                if (skor < 25) {
+                    status = "Gudang Perlu Peningkatan";
+                }
+            }
+        }
+        System.out.println("Informasi Gudang :");
+        System.out.println(nama + "|" + kapasitas + "|" + jumlah + "|" + aC + "|" + lantai + "|" + ketersediaan + "|" + skor + "|" + status);
+    }
+}
